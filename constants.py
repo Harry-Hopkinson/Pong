b8cff7fde189093630716ee23c7f36de36d772ff
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2021-11-29 15:44:57 +0000

    Uploaded the Main Script

---
+++ constants.py
@@ -0,0 +1,7 @@
+### Window ###
+WIDTH = 858
+HEIGHT = 525
+
+### Colours ###
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
---
+++ main.py
@@ -0,0 +1,56 @@
+### Image ###
+# https://www.google.com/search?q=create+pong+in+python+board&rlz=1C1GCEB_enGB978GB978&hl=en&tbm=isch&source=iu&ictx=1&fir=ZuRtlgqOLCAs3M%252C98ebYjHXFBlIaM%252C_&vet=1&usg=AI4_-kShZZJULG_Y_uqAxMsuOkKNIfh_RQ&sa=X&ved=2ahUKEwiCi6-Bx730AhWsQEEAHYAZCCIQ9QF6BAgFEAE&biw=786&bih=779&dpr=1&surl=1&safe=active&ssui=on#imgrc=ZuRtlgqOLCAs3M
+# https://www.pygame.org/docs/ref/draw.html#pygame.draw.circle
+
+### Imports ###
+import turtle
+from constants import *
+
+### Turtle Screen ###
+screen = turtle.Screen()
+screen.title("Pong")
+screen.bgcolor(WHITE)
+screen.setup(width=WIDTH, height=HEIGHT)
+
+class Game():
+    def __init__(self):
+        self.score = 0
+        self.ball = turtle.Turtle()
+        self.ball.speed(0)
+        self.ball.shape("circle")
+        self.ball.color(BLACK)
+        self.ball.penup()
+        self.ball.goto(0, 0)
+        self.ball.dx = 2
+        self.ball.dy = 2
+        self.paddles()
+        self.score_board()
+    
+    def paddles(self):
+        left_paddle = turtle.Turtle()
+        left_paddle.speed(0)
+        left_paddle.shape("square")
+        left_paddle.color(BLACK)
+        left_paddle.shapesize(stretch_wid=6, stretch_len=2)
+
+        right_paddle = turtle.Turtle()
+        right_paddle.speed(0)
+        right_paddle.shape("square")
+        right_paddle.color(BLACK)
+        right_paddle.shapesize(stretch_wid=6, stretch_len=2)
+
+    def game_over(self):
+        if self.ball.ycor() > HEIGHT:
+            self.ball.goto(0, 0)
+            self.ball.dx *= -1
+            self.score += 1
+            self.score_board()
+
+    def score_board(self):
+        self.score_a = turtle.Turtle()
+        self.score_a.speed(0)
+        self.score_a.color(BLACK)
+        self.score_a.penup()
+        self.score_a.setposition(-WIDTH/2 + 20, HEIGHT/2 - 20)
+        self.score_a.write(self.score, font=("Arial", 20, "normal"))
+
